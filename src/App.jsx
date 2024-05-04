d5105f16abfdcb097717fb1640c06f8e18b79cce
Speakiez <aerholgocalin57@gmail.com>
2024-05-04 23:30:41 +0800

    Add Board & Tile Component with styling
    I forgot to commit some changes xD

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,48 @@
+import { useState } from "react";
+
+function Tile() {
+    const [value, setValue] = useState(null);
+
+    const handleClick = event => setValue("X")
+
+    return (
+        <button 
+            className="board-tile"
+            onClick={handleClick}
+        >
+            {value}    
+        </button>
+    )
+}
+
+function Board() {
+    return (
+        <>  
+            <div className="board-row">
+                <Tile />
+                <Tile />
+                <Tile />
+            </div>
+            <div className="board-row">
+                <Tile />
+                <Tile />
+                <Tile />
+            </div>
+            <div className="board-row">
+                <Tile />
+                <Tile />
+                <Tile />
+            </div>
+        </>
+    );
+}
 
 function App() {
-  
+    return (
+        <>
+           <Board />
+        </>
+    );
 }
 
 export default App
